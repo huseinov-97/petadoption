b85a1af260dbfceb7ff0aa7fc6857ede1133cb28
huseinov-97 <mhuseinov7@gmail.com>
2020-10-23 03:06:11 +0200

    zuul enabled

--- api-gateway/src/main/java/com/bme/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bme/apigateway/ApiGatewayApplication.java
@@ -1,11 +1,16 @@
 package com.bme.apigateway;
 
 
+import com.bme.common.ApiDocsConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Import;
 
 
+@EnableZuulProxy
 @SpringBootApplication
+@Import(ApiDocsConfiguration.class)
 public class ApiGatewayApplication {
 
 		public static void main(String[] args) {
